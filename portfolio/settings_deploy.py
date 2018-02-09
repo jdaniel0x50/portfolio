37237abf4da7bcd5652adb569667755968b9b35e
Joshua Daniel <joshuadaniel@fuller.edu>
2018-02-09 14:00:03 -0600

    adding image upload to amazon s3

--- portfolio/settings_deploy.py
+++ portfolio/settings_deploy.py
@@ -12,8 +12,8 @@ ALLOWED_HOSTS = ['*']
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 # STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 DEFAULT_FILE_STORAGE = 'portfolio.storage_backends.MediaStorage'
-MEDIA_URL = 'https://s3.us-east-2.amazonaws.com/' + AWS_STORAGE_BUCKET_NAME + '/assets/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/assets/'
+MEDIA_ROOT = 'https://s3.us-east-2.amazonaws.com/' + AWS_STORAGE_BUCKET_NAME
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
