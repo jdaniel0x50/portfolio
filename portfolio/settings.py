d72ef50467e68db5ef70ec731a19c4ee33f743cc
Joshua Daniel <joshuadaniel@fuller.edu>
2018-02-10 22:57:18 -0600

    fixing email messaging

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -56,6 +56,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'apps/main/templates/email')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
