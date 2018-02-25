7cf96d338077bb6915360b67ea2215409243faba
Joshua Daniel <joshuadaniel@fuller.edu>
2018-02-24 18:35:44 -0600

    removed traffic and resume from admin.py

--- apps/db_admin/admin.py
+++ apps/db_admin/admin.py
@@ -2,8 +2,3 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from ._resume.models import Resume
-from ._traffic.models import Traffic
-
-admin.site.register(Resume)
-admin.site.register(Traffic)
