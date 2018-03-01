f363fdd4da88dcc653cb6340df412a04002c9d48
Joshua Daniel <joshuadaniel@fuller.edu>
2018-03-01 15:54:51 -0600

    changed permission required to users passes test

--- apps/db_admin/_traffic/views.py
+++ apps/db_admin/_traffic/views.py
@@ -2,11 +2,12 @@
 from __future__ import unicode_literals
 from django.shortcuts import render, redirect, get_list_or_404
 from ...main.exceptions import const, method_not_allowed
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+
 from datetime import datetime
 
 from .models import Traffic
+from ..models import superuser_or_admin
 
 def traffic_index(request, sort_f="none"):
     if not request.user.is_authenticated():
@@ -23,7 +24,8 @@ def traffic_index(request, sort_f="none"):
     return render(request, "db_traffic/index.html", context)
 
 @login_required
-@permission_required('db_admin.traffic.can_add_traffic', raise_exception=True)
+# @permission_required('db_admin.traffic.can_add_traffic', raise_exception=True)
+@user_passes_test(superuser_or_admin)
 def test_perm(request):
     return render(request, 'db_traffic/test.html')
     
