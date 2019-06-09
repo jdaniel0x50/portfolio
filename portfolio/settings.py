14a61efbcd7907ed8022652bdfccf3be70ab0cf1
jdaniel <joshua.daniel@uptake.com>
2019-06-09 18:55:26 -0500

    add django core request context processor

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.core.context_processors.request'
             ],
         },
     },
